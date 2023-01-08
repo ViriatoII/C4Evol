23d4bec513b3751d156f38d24ba3181525d4cd3c
ViriatusII <ricardoguerreiro21@hotmail.com>
2023-01-08 18:37:54 +0100

    add assembly scripts

--- ortho_analysis/01_astral.sh
+++ ortho_analysis/01_astral.sh
@@ -14,8 +14,5 @@ cat   $ortho_run/Trees_ids/OG00* > all_ogs.tre
 cat  all_ogs.tre | tr ":" "\n"  | head -200  |grep "_" | sed 's/.*(\|.*,//' | sed 's/\(.*\)_\(.*\)_/\1  \1_\2_/'  | sort   > gene_sp_translation
 
 
-sed 's/_.._[0-9][0-9]*-RA//g ; s/_HI3_[0-9][0-9]*-RA//g'  all_ogs.tre > all_ogs.trees 
-
-
 java -D"java.library.path=src/ASTRAL/A-pro/ASTRAL-MP/lib" -jar src/ASTRAL/A-pro/ASTRAL-MP/astral.1.1.5.jar \
--a ../gene_name_translations2.tsv -i all_ogs.trees -t1 -o all_ogs.final.tree  2>out.log
+-a ../gene_name_translations.tsv -i all_ogs.trees -t1 -o all_ogs.final.tree  2>out.log
